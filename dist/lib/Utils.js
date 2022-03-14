03b6b09d49d3ea329970d1e30247bb577739af62
ssMMiles <github@milesmoonlove.com>
2022-03-14 00:26:42 +0000

    restore gitignore

--- dist/lib/Utils.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getId = void 0;
-function getId(value) {
-    return typeof value === "string" ? value : value.id;
-}
-exports.getId = getId;
